22885e4d7ec43cbaf7d528b1e37bb5bb753ab894
Connor Runyan <connor.runyan@aggiemail.usu.edu>
2024-01-27 09:14:53 -0700

    Starting with DjangoX project template

---
+++ openai_client.py
@@ -0,0 +1,24 @@
+import os
+import asyncio
+from openai import AsyncOpenAI
+
+client = AsyncOpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+
+async def main() -> None:
+    chat_completion = await client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": "Say this is a test",
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+    breakpoint()
+
+
+asyncio.run(main())
